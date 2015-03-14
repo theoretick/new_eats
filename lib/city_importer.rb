35cb111eccd0a44a40bd1ed4b6b3e88b6ab2b86d
theoretick <me@lucascharles.me>
2015-03-14 11:30:21 -0700

    unproject specialized geometry on oregon cities shapefile import

--- lib/city_importer.rb
+++ lib/city_importer.rb
@@ -15,25 +15,34 @@ module CityImporter
   end
 
   class Importer
-    def self.execute(shapefile_path)
-      RGeo::Shapefile::Reader.open(shapefile_path) do |file|
-        file.each do |record|
-          _city =  City.new(name: record.attributes["CITY_NAME"])
+    def self.execute(shapefile_path, opts = default_opts)
+      City.delete_all unless opts[:update_only]
 
-          boundaries = record.geometry.reduce([]) do |all_boundaries, geom|
+      factory = RGeo::Geographic.projected_factory(:projection_proj4 => "+proj=aea +lat_1=43 +lat_2=48 +lat_0=34 +lon_0=-120 +x_0=600000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs ")
+      RGeo::Shapefile::Reader.open(shapefile_path, factory: factory.projection_factory) do |file|
+        file.each do |record|
+          attributes = record.attributes
+          _city =  City.where(name: attributes["CITYNAME"]).first_or_initialize
+
+          unprojected_geometry = factory.unproject(record.geometry)
+
+          boundaries = unprojected_geometry.reduce([]) do |all_boundaries, geom|
             all_boundaries << CityBoundary.new(city: _city, boundary: geom)
-            all_boundaries
           end
 
           _city.city_boundaries = boundaries
 
           if _city.save
-            puts "Saving City: #{_city.name} w/ #{_city.boundaries.size} boundaries"
+            puts "Attached City: #{_city.name} to #{_city.boundaries.size} boundaries"
           end
         end
         file.rewind
         record = file.next
       end
+    end
+
+    def self.default_opts
+      {update_only: false}
     end
   end
 end
